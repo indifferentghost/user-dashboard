7c173ef8ff0d4bab180cfcb69d29a58415d8c839
Thomas Dillard <thomas.dillard.studios@gmail.com>
2021-06-27 12:06:30 -0700

    With a choose your own JSON adventure

--- routes/main.js
+++ routes/main.js
@@ -1,9 +1,25 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.render('index');
+  res.render("index");
+});
+
+const JSON = {
+  name: "100Devs",
+  address: "remote",
+  type: "school",
+};
+
+router.get("/choose", (req, res) => {
+  console.log(req.headers.accept);
+  const isJSONRequest = req.headers.accept.includes("application/json");
+  if (isJSONRequest) {
+    res.json(JSON);
+    return;
+  }
+  res.render("choose", JSON);
 });
 
 router.get((req, res) => {
