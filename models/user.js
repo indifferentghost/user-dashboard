778c6ac2b7972363be9b967ea88bc463c1113f00
Thomas Dillard <thomas.dillard.studios@gmail.com>
2021-07-03 11:37:36 -0700

    Remove notes because it's cleaner!

--- models/user.js
+++ models/user.js
@@ -13,32 +13,9 @@ const UserSchema = mongoose.Schema({
   }
 }, { timestamps: true });
 
-/*
-  // assign a function to the "methods" object of our animalSchema
-  animalSchema.methods.findSimilarTypes = function(cb) {
-    return mongoose.model('Animal').find({ type: this.type }, cb);
-  };
-*/
-// instance methods
-// <- child type `user`
-// user.findW/e
-// validate the password
-// password on the individual user
-
-// `this` is hard
-// congitive load
-// user.password
 UserSchema.methods.comparePasswords = function(passwordToTest) {
   return bcrypt.compareSync(passwordToTest, this.password);
 }
-/* 
-// why we're using a function declaration instead of an arrow function
-const userInstance = User.find({ _id: id });
-
-const { comparePasswords } = userInstace;
-
-comparePasswords()
-*/
 
 UserSchema.static.hashPassword = (password) => {
   return bcrypt.hashSync(password, bcrypt.genSaltSync(10));
